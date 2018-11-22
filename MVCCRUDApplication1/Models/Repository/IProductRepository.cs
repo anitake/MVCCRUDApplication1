84c8c8ec150a171cb120c4c66c72dd6dfd3b9f71
Anita <s_anitake@yahoo.com.tw>
2018-11-22 10:00:44 +0800

    加入Modles

---
+++ MVCCRUDApplication1/Models/Repository/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCCRUDApplication1.Models.Repository
+{
+    public interface IProductRepository
+    {
+        IQueryable<Product> GetAll();
+
+        Product GetById(int id);
+
+        void Create(Product product);
+
+        void Update(Product product);
+
+        void Delete(Product product);
+
+    }
+}
---
+++ MVCCRUDApplication1/Models/Repository/ProductRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCCRUDApplication1.Models.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        protected storeEntities fDb
+        {
+            get;
+            private set;
+        }
+
+        public ProductRepository()
+        {
+            this.fDb = new storeEntities();
+        }
+
+        public void Create(Product product)
+        {
+            if (product == null)
+            {
+                throw new ArgumentNullException("product");
+            }
+            else
+            {
+                fDb.Products.Add(product);
+                fDb.SaveChanges();
+            }
+        }
+
+        public void Delete(Product product)
+        {
+            if (product == null)
+            {
+                throw new ArgumentNullException("product");
+            }
+            else
+            {
+                fDb.Entry(product).State = EntityState.Deleted;
+                fDb.SaveChanges();
+            }
+        }
+
+        public IQueryable<Product> GetAll()
+        {
+            return fDb.Products.AsQueryable();
+        }
+
+        public Product GetById(int id)
+        {
+            return fDb.Products.Find(id);
+        }
+
+        public void Update(Product product)
+        {
+            if (product == null)
+            {
+                throw new ArgumentNullException("product");
+            }
+            else
+            {
+                fDb.Entry(product).State = EntityState.Modified;
+                fDb.SaveChanges();
+            }
+        }
+    }
+}
